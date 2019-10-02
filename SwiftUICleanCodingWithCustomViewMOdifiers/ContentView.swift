5c1d6a2eb1433faa2638485f9dd66ad6f6dffaa8
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 20:25:05 +0300

    three buttons

--- SwiftUICleanCodingWithCustomViewMOdifiers/ContentView.swift
+++ SwiftUICleanCodingWithCustomViewMOdifiers/ContentView.swift
@@ -30,6 +30,44 @@ struct ContentView: View {
                     
                     
                 })
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("More Details")
+                        .foregroundColor(.white)
+                        .font(.system(size: 16))
+                        .padding(.horizontal, 14)
+                        .padding(.vertical, 10)
+                        .background(Color.green)
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 3)
+                        .strokeBorder(style: StrokeStyle(lineWidth: 1))
+                        .foregroundColor(Color(.sRGB, red: 0.1, green: 0.1, blue: 0.1, opacity: 1)))
+                    .cornerRadius(4)
+                        .shadow(color: .init(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 5, x: 0, y: 0)
+                    
+                    
+                })
+                
+                Button(action: {
+                    
+                }, label: {
+                    Text("Cancel")
+                        .foregroundColor(.white)
+                        .font(.system(size: 16))
+                        .padding(.horizontal, 14)
+                        .padding(.vertical, 10)
+                        .background(Color.red)
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 3)
+                        .strokeBorder(style: StrokeStyle(lineWidth: 1))
+                        .foregroundColor(Color(.sRGB, red: 0.1, green: 0.1, blue: 0.1, opacity: 1)))
+                    .cornerRadius(4)
+                        .shadow(color: .init(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 5, x: 0, y: 0)
+                    
+                    
+                })
             }
         }
     }

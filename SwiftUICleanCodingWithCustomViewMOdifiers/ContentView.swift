63ec521b7d8369b24900c99b636de3c80b66bb7b
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 20:16:30 +0300

    style

--- SwiftUICleanCodingWithCustomViewMOdifiers/ContentView.swift
+++ SwiftUICleanCodingWithCustomViewMOdifiers/ContentView.swift
@@ -16,6 +16,19 @@ struct ContentView: View {
                     
                 }, label: {
                     Text("Continue")
+                        .foregroundColor(.white)
+                        .font(.system(size: 16))
+                        .padding(.horizontal, 14)
+                        .padding(.vertical, 10)
+                        .background(Color.blue)
+                    .overlay(
+                    RoundedRectangle(cornerRadius: 3)
+                        .strokeBorder(style: StrokeStyle(lineWidth: 1))
+                        .foregroundColor(Color(.sRGB, red: 0.1, green: 0.1, blue: 0.1, opacity: 1)))
+                    .cornerRadius(4)
+                        .shadow(color: .init(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 5, x: 0, y: 0)
+                    
+                    
                 })
             }
         }

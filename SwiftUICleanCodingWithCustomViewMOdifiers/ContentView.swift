2ebbe3b9040f6baa6ce839c5e716c73031da4c97
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-02 20:10:37 +0300

    init

--- SwiftUICleanCodingWithCustomViewMOdifiers/ContentView.swift
+++ SwiftUICleanCodingWithCustomViewMOdifiers/ContentView.swift
@@ -10,7 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack() {
+            HStack {
+                Button(action: {
+                    
+                }, label: {
+                    Text("Continue")
+                })
+            }
+        }
     }
 }
 
